03cabaffe19551ecfba83df2db43fa5a9ca54ec9
Bérces Zsombor <bercikezs199999999@gmail.com>
2018-02-10 14:05:14 +0100

    neon.html, neon js, products js

--- slides.js
+++ slides.js
@@ -27,4 +27,51 @@ window.onload = function() {
     
     startSlider();
   });
-    }
+  
+    $(function () {
+  
+  var height = 270;
+  var animationSpeed = 1500;
+  var imgTime = 5000;
+  var currentSlide = 1;
+  
+  var $slideCont = $('.neonSlideContainer');
+  var $fullSlide = $slideCont.find('.neonFullSlide');
+  var $slides = $fullSlide.find('.neonSlides');
+  
+  var interval;
+  
+  function neonStartSlider() {
+    interval = setInterval(function() {
+      $fullSlide.animate({marginTop: '-=' + height}, animationSpeed, function() {
+      currentSlide++;
+        if (currentSlide === $slides.length) {
+          currentSlide = 1;
+          $fullSlide.css({marginTop: 0});
+        };
+      });
+    }, imgTime);
+  };
+    
+    neonStartSlider();
+  });
+  
+    var num = 1;
+    
+    $('.termekek').on('click', function() { 
+      
+      if (num == 1) {
+        $('.menuWrapperTermekek').addClass('menuWrapperActive');
+        $('.termek').addClass('termekActive'); 
+        num++;
+      } else {
+              
+        $('.menuWrapperTermekek').removeClass('menuWrapperActive');
+        $('.termek').removeClass('termekActive'); 
+        num--;
+      }
+    });
+  
+}
+
+  
